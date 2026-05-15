4427d1690dd74c7fa273e8cfb6371a032d990550
bchraaaa2-arch <bchraaaa2@gmail.com>
2026-05-15 17:28:44 +0100

    Update bus.js

--- src/pages/bus.js
+++ src/pages/bus.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Layout from '../components/Layout';
+import { mockData } from '../data/mockData';
 import { useData } from '../context/DataContext';
 
 export default function Bus() {
